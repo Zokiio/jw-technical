1e938bc0c6da9a66d94283009be8a2bfa0f10a15
Zokiio <joakim.hall1@gmail.com>
2021-05-19 22:22:52 +0900

    Refactored validators to Validators.cs

--- jw-robot/Helpers.cs
+++ jw-robot/Helpers.cs
@@ -4,33 +4,15 @@ namespace jw_robot
 {
     public static class Helpers
     {
-        public static void ClearCurrentConsoleLine()
+        public static void ClearCurrentConsoleLine(int lines = 1)
         {
             var currentLineCursor = Console.CursorTop;
-            Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(new string(' ', Console.WindowWidth)); 
+            for (var i = 1; i <= lines; i++)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(new string(' ', Console.WindowWidth)); 
+            }
             Console.SetCursorPosition(0, currentLineCursor);
-        }
-
-        public static bool TryParseIntList(string input, out int[] inputValues, int length = 0)
-        {
-            inputValues = default;
-            var splits = input.Split(" ");
-            var result = new int[splits.Length];
-            for (int i = 0; i < splits.Length; i++)
-            {
-                if (!int.TryParse(splits[i].Trim(), out result[i]))
-                {
-                    return false;
-                }
-            }
-            if (length != 0 && result.Length != length)
-            {
-                return false;
-            }
-
-            inputValues = result;
-            return true;
         }
     }
 }
---
+++ jw-robot/Validators.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Enum;
+
+namespace jw_robot
+{
+    public static class Validator
+    {
+        public static bool ValidateInstructions(string input, out Instruction[] instructions)
+        {
+            instructions = default;
+            var split = input.Trim().Replace(" ", "").ToUpper().ToCharArray();
+            var temp = new Instruction[split.Length];
+            for (var i = 0; i < split.Length; i++)
+            {
+                if(!TryParse(split[i].ToString(), out temp[i]))
+                {
+                    return false;
+                }
+            }
+
+            instructions = temp;
+            return true;
+        }
+        
+        public static bool ValidateInputStartPosition(string input, Position values,(int x, int y) fieldSize)
+        {
+            var split = input.Trim().ToUpper().Split();
+            if (split.Length != 3)
+            {
+                return false;
+            }
+            
+            var parseX = int.TryParse(split[0], out values.X);
+            var parseY = int.TryParse(split[1], out values.Y);
+            var parseF = Enum.TryParse(split[2], out values.Facing);
+            var (x, y) = fieldSize;
+            if(values.X > x || values.Y > y)
+            {
+                return false;
+            }
+            return parseX || parseY || parseF;
+        }
+        
+        public static bool TryParseIntList(string input, out int[] inputValues, int length = 0)
+        {
+            inputValues = default;
+            var splits = input.Split(" ");
+            var result = new int[splits.Length];
+            for (var i = 0; i < splits.Length; i++)
+            {
+                if (!int.TryParse(splits[i].Trim(), out result[i]))
+                {
+                    return false;
+                }
+            }
+            if (length != 0 && result.Length != length)
+            {
+                return false;
+            }
+
+            inputValues = result;
+            return true;
+        }
+    }
+}
