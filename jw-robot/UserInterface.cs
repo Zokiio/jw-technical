b5afc138f225a59329dc6b44dfa4a475d72361f3
Zokiio <joakim.hall1@gmail.com>
2021-05-19 22:24:07 +0900

    Add Validation on input

--- jw-robot/UserInterface.cs
+++ jw-robot/UserInterface.cs
@@ -1,14 +1,13 @@
 ﻿using System.Drawing;
-using System.Linq;
-using static System.Enum;
+
 using Console = Colorful.Console;
 
 namespace jw_robot
 {
     public class UserInterface
     {
-        private int startRow = 12;
-        private int startCol = 0;
+        private int _startRow = 12;
+        private int _startCol = 0;
         
         public void WelcomeTitle(string title)
         {
@@ -22,78 +21,75 @@ namespace jw_robot
 
         private void UpdateValues(int row, string value)
         {
-            Console.SetCursorPosition(startCol + 19, row);
+            Console.SetCursorPosition(_startCol + 19, row);
             Console.Write(value);
         }
 
         public void AddFinalReport(Position finalPosition)
         {
-            Console.SetCursorPosition(startCol, startRow+11);
+            Console.SetCursorPosition(29, 9);
             Console.WriteLine($"Final Position: X: {finalPosition.X}, Y: {finalPosition.Y}, Facing: {finalPosition.Facing}", Color.Goldenrod);// Row  10
         }
 
         public (int x, int y) InputFieldSize()
         {
             int[] inputValues;
-            Console.SetCursorPosition(startCol, startRow);
+            Console.SetCursorPosition(_startCol, _startRow);
             Console.WriteFormatted("Enter field size. {0}", Color.YellowGreen, Color.SeaGreen,"(Ex: 5 6): ");
-            while (!Helpers.TryParseIntList(Console.ReadLine(), out inputValues, 2))
+            while (!Validator.TryParseIntList(Console.ReadLine().Trim(), out inputValues, 2))
             {
-                Console.SetCursorPosition(startCol, startRow);
+                Console.SetCursorPosition(_startCol, _startRow);
                 Helpers.ClearCurrentConsoleLine();
                 Console.WriteLine("Invalid Input. Try Again...", Color.Crimson);
                 Helpers.ClearCurrentConsoleLine();
-                Console.SetCursorPosition(startCol, startRow+1);
+                Console.SetCursorPosition(_startCol, _startRow+1);
                 Console.WriteFormatted("Enter field size. {0}", Color.YellowGreen, Color.SeaGreen,"(Ex: 5 6): ");
             }
 
             UpdateValues(8, $"{inputValues[0]} {inputValues[1]}");
             return (inputValues[0], inputValues[1]);
         }
-        
-        public Position InputStartPosition()
+
+        public Position InputStartPosition((int x, int y) fieldSize)
         {
-            string[] input;
-            int[] parsed;
             var inputValues = new Position();
             
-            Console.SetCursorPosition(startCol, startRow);
-            Console.WriteFormatted("Enter Starting position and what direction the robot should face. \nUse {0} for direction {1} ",Color.YellowGreen, Color.SeaGreen,"N/E/S/W", "(Ex: 2 3 N): ");
-            
-            // TODO: Check Correct input
-            input = Console.ReadLine().Split();
-            while (!Helpers.TryParseIntList($"{input[0]} {input[1]}", out parsed, 2))
+            Console.SetCursorPosition(_startCol, _startRow);
+            Console.WriteLineFormatted("Enter Starting position and what direction the robot should face. \nUse {0} for direction {1} ",Color.YellowGreen, Color.SeaGreen,"N/E/S/W", "(Ex: 2 3 N): ");
+            Console.Write("Start position, X / Y, can't be lower or larger than the field size: ",Color.SeaGreen);
+
+            while (!Validator.ValidateInputStartPosition(Console.ReadLine(), inputValues, fieldSize))
             {
-                Console.SetCursorPosition(startCol, startRow+1);
+                Console.SetCursorPosition(_startCol, _startRow);
                 Helpers.ClearCurrentConsoleLine();
                 Console.WriteLine("Invalid Input. Try Again...", Color.Crimson);
                 Helpers.ClearCurrentConsoleLine();
-                Console.SetCursorPosition(startCol, startRow+2);
-                Console.WriteFormatted("Enter Starting position and what direction the robot should face. \nUse {0} for direction {1} ",Color.YellowGreen, Color.SeaGreen,"N/E/S/W", "(Ex: 2 3 N): ");
-                Console.ReadKey();
-                break;
+                Console.SetCursorPosition(_startCol, _startRow+1);
+                Console.WriteLineFormatted("Enter Starting position and what direction the robot should face. \nUse {0} for direction {1} ",Color.YellowGreen, Color.SeaGreen,"N/E/S/W", "(Ex: 2 3 N): ");
+                Console.Write("Start position, X / Y, can't be lower or larger than the field size: ", Color.SeaGreen);
             }
-
-            TryParse(input[2], out inputValues.Facing);
-            inputValues.X = parsed[0];
-            inputValues.Y = parsed[1];
-            UpdateValues(9, $"{input[0]} {input[1]} {input[2]}");
+            
+            UpdateValues(9, $"{inputValues.X} {inputValues.Y} {inputValues.Facing}");
             return inputValues;
         }
 
-        public char[] InputInstructions()
+        public Instruction[] InputInstructions()
         {
-            Console.SetCursorPosition(startCol, startRow);
-            Helpers.ClearCurrentConsoleLine();
+            Instruction[] instructions;
+            Console.SetCursorPosition(_startCol, _startRow);
+            Helpers.ClearCurrentConsoleLine(5);
             Console.WriteLine("Enter move instructions for the robot.",Color.SeaGreen);
             Console.WriteFormatted("Use {0} to turn Right, {1} to turn Left, {2} to walk Froward. {3}",Color.YellowGreen, Color.SeaGreen, new string[]{"R","L","F","(Ex: FRFFLFLLF):"} );
-            
-            var input = Console.ReadLine();
-            // TODO: Typecheck input
-            UpdateValues(10, input);
-            var split = input.ToCharArray();
-            
-            return split;
+
+            while (!Validator.ValidateInstructions(Console.ReadLine(), out instructions))
+            {
+                Console.SetCursorPosition(_startCol, _startRow);
+                Helpers.ClearCurrentConsoleLine(5);
+                Console.WriteLine("Enter move instructions for the robot.",Color.SeaGreen);
+                Console.WriteFormatted("Use {0} to turn Right, {1} to turn Left, {2} to walk Froward. {3}",Color.YellowGreen, Color.SeaGreen, new string[]{"R","L","F","(Ex: FRFFLFLLF):"} );
+            }
+
+            return instructions;
         }
     }
 }
