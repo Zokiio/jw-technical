0c48e5b7d8efb0744c690a3633e04d150956131d
Zokiio <joakim.hall1@gmail.com>
2021-05-19 17:02:26 +0900

    Add Robot class

---
+++ jw-robot/Robot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+namespace jw_robot
+{
+    public class Robot
+    {
+        public Position Position { get; set; }
+        public (int x, int y) CurrentField { get; set; }
+
+        public Robot(Position position, (int x, int y) currentField)
+        {
+            Position = position;
+            CurrentField = currentField;
+        }
+
+        public void Move(char[] instructions)
+        {
+            foreach (var movement in instructions)
+            {
+                switch (movement)
+                {
+                    case 'F':
+                        Console.WriteLine(movement);
+                        break;
+                    case 'L':
+                        Console.WriteLine(movement);
+                        break;
+                    case 'R':
+                        Console.WriteLine(movement);
+                        break;
+                    default:
+                        Console.WriteLine("Unexpected value");
+                        break;
+                }
+            }
+        }
+    }
+}
