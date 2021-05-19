2bcb0e33e91fa9ffb2c2e8911b9fce48d76e4b1d
Zokiio <joakim.hall1@gmail.com>
2021-05-19 23:47:35 +0900

    Add Field.cs

---
+++ jw-robot/Field.cs
@@ -0,0 +1,14 @@
+﻿namespace jw_robot
+{
+    public class Field
+    {
+        public int Width { get; set; }
+        public int Depth { get; set; }
+
+        public Field(int width, int depth)
+        {
+            Width = width;
+            Depth = depth;
+        }
+    }
+}
