50af63a17ec44227b8877bf8e65321c5939ebcb3
Zokiio <joakim.hall1@gmail.com>
2021-05-19 16:42:26 +0900

    Add Position Struct

---
+++ jw-robot/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace jw_robot
+{
+    public struct Position
+    {
+        public int X;
+        public int Y;
+        public string Facing;
+        
+        public Position(int x, int y, string facing)
+        {
+            X = x;
+            Y = y;
+            Facing = facing;
+        }
+    }
+}
