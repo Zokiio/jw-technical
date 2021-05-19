3903b7c75a92186376bda3636da6ce8075965d58
Zokiio <joakim.hall1@gmail.com>
2021-05-19 22:26:18 +0900

    Add project files

---
+++ jw-robot/Program.cs
@@ -0,0 +1,23 @@
+﻿using Console = Colorful.Console;
+
+namespace jw_robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInterface = new UserInterface();
+
+            userInterface.WelcomeTitle("JW Robot");
+            var fieldSize = userInterface.InputFieldSize();
+            var startingPos = userInterface.InputStartPosition(fieldSize);
+            var instructions = userInterface.InputInstructions();
+
+            var robot = new Robot(startingPos, fieldSize);
+            var robotPosition = robot.Move(instructions);
+            userInterface.AddFinalReport(robotPosition);
+
+            Console.ReadKey();
+        }
+    }
+}
